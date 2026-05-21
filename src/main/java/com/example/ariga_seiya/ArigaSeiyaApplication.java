96879caaf4d1bd19e397020da1d621b55bb49aa9
ariga_seiya <ariga.seiya@alhinc.jp>
2026-05-21 17:38:54 +0900

    first commit

---
+++ src/main/java/com/example/ariga_seiya/ArigaSeiyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ariga_seiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArigaSeiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArigaSeiyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ariga_seiya/ArigaSeiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ariga_seiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArigaSeiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
